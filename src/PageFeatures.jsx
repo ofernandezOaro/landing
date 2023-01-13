9fe6f5c436164485eed899ad0a9c0f55403d18b2
ofernandezOaro <ofernandez@oaro.net>
2023-01-13 18:45:24 +0100

    Layout pdf features"

--- src/PageFeatures.jsx
+++ src/PageFeatures.jsx
@@ -7,22 +7,26 @@ const PageFeatures = () => {
       <div className='container'>
         <div className='container__title'>
           <div className='container__title_items'>
-          <div className='line'/>
-          <h2>What is A.R.S.?</h2>
+            <div className='line'/>
+            <h2>What is A.R.S.?</h2>
           </div>
           <span>An unprecedented audiobook recording system, created by and for audiobook professionals, to transform the full production cycle.</span>
+          <hr className='hr'/>
         </div>
         <div className='container__features'>
-          <div>Team agenda clashes</div>
-          <div>Limited opportunities to scale</div>
-          <div>High costs due to staff hours</div>
-          <div>Limited access to resources</div>
-          <div>Multiple manual processes</div>
-          <div>Limited studio availability</div>
+          <div className='features_title'>It solves key production bottle necks:</div>
+          <div className='features'>
+            <div>Team agenda clashes</div>
+            <div>Limited opportunities to scale</div>
+            <div>High costs due to staff hours</div>
+            <div>Limited access to resources</div>
+            <div>Multiple manual processes</div>
+            <div>Limited studio availability</div>
+          </div>
+          <h3>We had to find a way to reduce these pains, and we did it.</h3>
         </div>
         <br/>
         <br/>
-        <h3>We had to find a way to reduce these pains, and we did it.</h3>
       </div>
     </div>
   )
